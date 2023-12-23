ffc6749cae698cc5dab13c60a742dcb244ab8b53
RealLionelMuhire <muhirelionel@gmail.com>
2023-12-23 11:08:47 +0200

    updating client form

--- src/scenes/clients/index.jsx
+++ src/scenes/clients/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
@@ -14,33 +14,20 @@ const Clients = () => {
   const { data, isLoading, refetch } = useGetClientsQuery();
   const [activateClient, { isLoading: isActivating }] = useActivateClientMutation();
   const [deactivateClient, { isLoading: isDeactivating }] = useDeactivateClientMutation();
-
+  
   const navigate = useNavigate();
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [selectedClientIds, setSelectedClientIds] = useState([]);
 
-  const handleSelectionChange = (id) => {
-    setSelectedClientIds((prevSelected) => {
-      if (prevSelected.includes(id)) {
-        return prevSelected.filter((selectedId) => selectedId !== id);
-      } else {
-        return [...prevSelected, id];
-      }
-    });
-  };
-
   const handleActivateClick = async (activate) => {
     try {
-      // Map over selectedClientIds and activate or deactivate each client
       const promises = selectedClientIds.map(async (clientId) => {
         await activateClient(clientId);
       });
-      // Wait for all promises to resolve
       await Promise.all(promises);
 
-      // Activation or deactivation successful
-      await refetch(); // Refetch the client list after activation or deactivation
+      await refetch();
     } catch (error) {
       // Handle error
       console.error("Error activating/deactivating clients:", error);
@@ -71,26 +58,19 @@ const Clients = () => {
     });
   };
 
+  const handleSelectionModelChange = (selectionModel) => {
+    setSelectedClientIds(selectionModel);
+  };
+
   const columns = [
-    {
-      field: "selection",
-      headerName: "Select",
-      renderCell: (params) => (
-        <Checkbox
-          checked={selectedClientIds.includes(params.id)}
-          onChange={() => handleSelectionChange(params.id)}
-        />
-      ),
-      flex: 0.5,
-    },
     { field: "id", headerName: "ID", flex: 0.5 },
-    { field: "FirstName", headerName: "First Name", flex: 1, cellClassName: "name-column--cell" },
-    { field: "LastName", headerName: "Last Name", flex: 1 },
+    { field: "firstName", headerName: "First Name", flex: 1, cellClassName: "name-column--cell" },
+    { field: "lastName", headerName: "Last Name", flex: 1 },
     { field: "passportIdNumber", headerName: "Passport ID Number", flex: 1 },
     { field: "countryOfIssue", headerName: "Country of Issue", flex: 1 },
-    { field: "clientContactPhone", headerName: "Client Contact Phone", flex: 1 },
+    { field: "clientContact", headerName: "Client Contact Phone", flex: 1 },
     { field: "clientEmail", headerName: "Client Email", flex: 1 },
-    { field: "preferredLanguage", headerName: "Preferred Language", flex: 1 },
+    { field: "preferredLanguage", headerName: "Language", flex: 1 },
     { field: "isActive", headerName: "Is Active", flex: 1 },
   ];
 
@@ -116,10 +96,10 @@ const Clients = () => {
             type="button"
             color="secondary"
             variant="contained"
-            onClick={() => handleActivateClick(false)}
+            onClick={handleActivateClick}
             disabled={selectedClientIds.length !== 1 || isActivating}
           >
-            Select To Activate
+            Activate Selected
           </Button>
         </Box>
         <Box display="flex" justifyContent="end" mt="20px" >
@@ -128,10 +108,10 @@ const Clients = () => {
             type="button"
             color="secondary"
             variant="contained"
-            onClick={() => handleDeactivateClick()}
-        disabled={selectedClientIds.length !== 1 || isDeactivating}
+            onClick={handleDeactivateClick}
+            disabled={selectedClientIds.length !== 1 || isDeactivating}
           >
-            Select To Deactivate
+            Deactivate Selected
           </Button>
         </Box>
         <Box display="flex" justifyContent="end" mt="20px">
@@ -176,11 +156,13 @@ const Clients = () => {
         }}
       >
         <DataGrid
+          checkboxSelection
           loading={isLoading || !data}
           getRowId={(row) => row.id}
           rows={data || []}
           columns={columns}
           components={{ Toolbar: GridToolbar }}
+          onSelectionModelChange={handleSelectionModelChange}
         />
       </Box>
     </Box>
