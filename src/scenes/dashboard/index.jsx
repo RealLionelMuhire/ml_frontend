dfa1ad911a046a63328d4c49e454258650ebd08a
RealLionelMuhire <muhirelionel@gmail.com>
2024-02-20 09:24:45 +0200

    updating dashboard data to include bookings

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -4,6 +4,7 @@ import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
+import MessageIcon from "@mui/icons-material/Traffic"
 import Header from "../../components/Header";
 import StatBox from "../../components/StatBox";
 import { useGetDashboardQuery } from "../../state/api";
@@ -53,7 +54,7 @@ const Dashboard = () => {
         gap="20px"
       >
         <Box
-          gridColumn="span 4"
+          gridColumn="span 3"
           backgroundColor={colors.primary[400]}
           display="flex"
           alignItems="center"
@@ -72,7 +73,7 @@ const Dashboard = () => {
           />
         </Box>
         <Box
-          gridColumn="span 4"
+          gridColumn="span 3"
           backgroundColor={colors.primary[400]}
           display="flex"
           alignItems="center"
@@ -91,7 +92,7 @@ const Dashboard = () => {
           />
         </Box>
         <Box
-          gridColumn="span 4"
+          gridColumn="span 3"
           backgroundColor={colors.primary[400]}
           display="flex"
           alignItems="center"
@@ -103,15 +104,37 @@ const Dashboard = () => {
             progress="#"
             increase="#"
             icon={
+              <MessageIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+          
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title={userDashboard.total_reservations}
+            subtitle="Reservations"
+            progress={userDashboard.increase_rate_reservations}
+            increase={`${userDashboard.increase_rate_reservations_percentage}%`}
+            icon={
               <TrafficIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
           />
+          
         </Box>
-
+        
+{/* recent services */}
         <Box
-          gridColumn="span 12"
+          gridColumn="span 6"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
           overflow="auto"
@@ -124,11 +147,16 @@ const Dashboard = () => {
             colors={colors.grey[100]}
             p="15px"
           >
-            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
+            <Typography color={colors.grey[100]} variant="h4" fontWeight="600">
               Recent Services
             </Typography>
           </Box>
           {userDashboard.recent_services.map((transaction, i) => (
+          <Box
+            flex="1"
+            paddingRight="15px"
+            borderRight={`4px solid ${colors.primary[500]}`}
+          >
             <Box
               key={`${transaction.txId}-${i}`}
               display="flex"
@@ -137,7 +165,11 @@ const Dashboard = () => {
               borderBottom={`4px solid ${colors.primary[500]}`}
               p="15px"
             >
-              <Box>
+              <Box
+                flex="1"
+                paddingRight="15px"
+                borderRight={`4px solid ${colors.primary[500]}`}
+              >
                 <Typography
                   color={colors.greenAccent[500]}
                   variant="h5"
@@ -145,18 +177,95 @@ const Dashboard = () => {
                 >
                   {transaction.client_name}
                 </Typography>
-                <Typography color={colors.grey[100]}>
+                <Typography color={colors.grey[100]} variant="h5">
                   {transaction.user}
                 </Typography>
               </Box>
-              <Box color={colors.grey[100]}>{transaction.date}</Box>
               <Box
-                backgroundColor={colors.greenAccent[500]}
-                p="5px 10px"
-                borderRadius="4px"
+                color={colors.grey[100]}
+                flex="1"
+                paddingLeft="15px"
+                borderRight={`4px solid ${colors.primary[500]}`}
               >
-                {transaction.currency}
-                {transaction.total_cost}
+                <Typography color={colors.grey[100]} variant="h5">
+                  {transaction.date}
+                </Typography>
+              </Box>
+              <Box
+                flex="1"
+                paddingLeft="15px"
+              >
+                <Typography variant="h5">
+                  {transaction.currency}
+                  {" "}{transaction.total_cost}
+                </Typography>
+              </Box>
+            </Box>
+            </Box>
+          ))}
+        </Box>
+
+        {/* recent bookings */}
+
+        <Box
+          gridColumn="span 6"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          overflow="auto"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid ${colors.primary[500]}`}
+            colors={colors.grey[100]}
+            p="15px"
+          >
+            <Typography color={colors.grey[100]} variant="h4" fontWeight="600">
+              Next Bookings
+            </Typography>
+          </Box>
+          {userDashboard.oldest_reservations.map((booking, i) => (
+            <Box
+              key={`${booking.txId}-${i}`}
+              display="flex"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              p="15px"
+            >
+              <Box
+                flex="2.3"
+                paddingRight="15px"
+                borderRight={`4px solid ${colors.primary[500]}`} // Add this line
+              >
+                <Typography
+                  color={colors.greenAccent[500]}
+                  variant="h5"
+                  fontWeight="600"
+                >
+                  {booking.name}
+                </Typography>
+                <Typography color={colors.grey[100]}>
+                  {booking.user}
+                </Typography>
+              </Box>
+              <Box
+                flex="4.5"
+                paddingLeft="15px"
+                borderRight={`4px solid ${colors.primary[500]}`}
+              >
+                <Typography variant="h6">
+                  {booking.reserved_period}
+                </Typography>
+              </Box>
+              <Box
+                flex="3"
+                paddingLeft="15px"
+              >
+                <Typography variant="h5">
+                  {booking.service_to_discuss}
+                  {" "}{booking.other_services}
+                </Typography>
               </Box>
             </Box>
           ))}
