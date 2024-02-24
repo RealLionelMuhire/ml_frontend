c8315e86fa2697d6109851b0a2c461d027923643
RealLionelMuhire <muhirelionel@gmail.com>
2024-02-24 18:18:34 +0200

    updating the

---
+++ src/scenes/reservations_data/ClientPastReservations.jsx
@@ -0,0 +1,105 @@
+import {
+    Box,
+    useTheme,
+    Button,
+  } from "@mui/material";
+  import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+  import { tokens } from "../../theme";
+  import { useNavigate } from "react-router-dom";
+  import { useGetPastReservationsQuery } from "../../state/api";
+  
+  const ClientPastReservations = () => {
+    const { data, isLoading, refetch } = useGetPastReservationsQuery();
+    const navigate = useNavigate();
+  
+    if (data) {
+      console.log(data);
+    } else {
+      console.log("no data available")
+    }
+    
+  
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const columns = [
+      { field: "id", headerName: "ID", flex: 0.2 },
+      {
+        field: "fullName",
+        headerName: "Client",
+        flex: 1.5,
+      },
+      {
+        field: "clientContact",
+        headerName: "Contact",
+        flex: 1,
+      },
+      {
+        field: "email",
+        headerName: "Email",
+        flex: 1,
+      },
+      {
+        field: "reserved_period",
+        headerName: "Reserved On",
+        cellClassName: "name-column--cell",
+        flex: 1.7,
+      },
+      {
+        field: "servicesToDiscuss",
+        headerName: "services To Discuss",
+        flex: 2,
+      },
+      {
+        field: "otherServices",
+        headerName: "Others To Discuss",
+        flex: 1.3,
+      },
+    ];
+  
+    return (
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+        "& .MuiDataGrid-root": {
+            border: "none",
+        },
+        "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+        },
+        "& .name-column--cell": {
+            color: colors.greenAccent[300],
+        },
+        "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+        },
+        "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+        },
+        "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+        },
+        "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+        },
+        "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+        },
+        }}
+        >
+        <DataGrid
+        checkboxSelection
+        loading={isLoading || !data}
+        getRowId={(row) => row.id}
+        rows={data || []}
+        columns={columns}
+        components={{ Toolbar: GridToolbar }}
+        />
+      </Box>
+    );
+  };
+  
+  export default ClientPastReservations;
+  
--- src/scenes/reservations_data/index.jsx
+++ src/scenes/reservations_data/index.jsx
@@ -1,74 +1,35 @@
+import React, { useState } from "react";
 import {
   Box,
-  useTheme,
   Button,
 } from "@mui/material";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import { tokens } from "../../theme";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useGetFutureReservationsQuery } from "../../state/api";
+import ClientPastReservations from "./ClientPastReservations";
+import ClientFutureReservations from "./ClientFutureReservations";
 
-const ClientReservations = () => {
-  const { data, isLoading, refetch } = useGetFutureReservationsQuery();
-  const navigate = useNavigate();
+const ClientReservationsIndex = () => {
+  const [isPastReservations, setIsPastReservations] = useState(true);
 
-  if (data) {
-    console.log(data);
-  } else {
-    console.log("no data available")
-  }
-  
-
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const columns = [
-    { field: "id", headerName: "ID", flex: 0.2 },
-    {
-      field: "fullName",
-      headerName: "Client",
-      flex: 1.5,
-    },
-    {
-      field: "clientContact",
-      headerName: "Contact",
-      flex: 1,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      flex: 1,
-    },
-    {
-      field: "reserved_period",
-      headerName: "Reserved On",
-      cellClassName: "name-column--cell",
-      flex: 1.7,
-    },
-    {
-      field: "servicesToDiscuss",
-      headerName: "services To Discuss",
-      flex: 2,
-    },
-    {
-      field: "otherServices",
-      headerName: "Others To Discuss",
-      flex: 1.3,
-    },
-  ];
+  const toggleReservationsType = () => {
+    setIsPastReservations(!isPastReservations);
+  };
 
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="ClientReservations" subtitle="List of ClientReservations" />
+        <Header
+          title={`${isPastReservations ? "Past" : "Future"} Reservations`}
+          subtitle={`List of Clients ${isPastReservations ? "Past" : "Future"} Reservations`}
+        />
         <Box display="flex" justifyContent="left" mt="20px" ml="10px">
           <Button
             type="button"
             color="secondary"
             variant="contained"
+            onClick={toggleReservationsType}
           >
-            Go To Past Rereservations
+            Go To {isPastReservations ? "Future" : "Past"} Reservations
           </Button>
         </Box>
         <Box display="flex" justifyContent="end" mt="20px">
@@ -77,49 +38,11 @@ const ClientReservations = () => {
           </Button>
         </Box>
       </Box>
-      <Box
-        m="40px 0 0 0"
-        height="75vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .name-column--cell": {
-            color: colors.greenAccent[300],
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: colors.blueAccent[700],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: colors.primary[400],
-          },
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-            backgroundColor: colors.blueAccent[700],
-          },
-          "& .MuiCheckbox-root": {
-            color: `${colors.greenAccent[200]} !important`,
-          },
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-            color: `${colors.grey[100]} !important`,
-          },
-        }}
-      >
-        <DataGrid
-          checkboxSelection
-          loading={isLoading || !data}
-          getRowId={(row) => row.id}
-          rows={data || []}
-          columns={columns}
-          components={{ Toolbar: GridToolbar }}
-        />
+      <Box>
+        {isPastReservations ? <ClientPastReservations /> : <ClientFutureReservations />}
       </Box>
     </Box>
   );
 };
 
-export default ClientReservations;
+export default ClientReservationsIndex;
