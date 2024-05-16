800aaf354e62c281f85ab451fac921d42543393e
RealLionelMuhire <RealLionel>
2024-05-16 05:15:22 +0200

    adding webpacks

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+resolve: {
+    extensions: ['.js', '.json'], // Add other extensions if needed
+    alias: {
+        'pdfjs-dist/build/pdf.worker.entry': 'pdfjs-dist/build/pdf.worker.js'
+    }
+}
+
