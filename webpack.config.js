f787f6bb36817346e67db03648b26b301bac5a14
RealLionelMuhire <RealLionel>
2024-05-16 05:36:31 +0200

    adding webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,43 @@
-resolve: {
-    extensions: ['.js', '.json'], // Add other extensions if needed
-    alias: {
-        'pdfjs-dist/build/pdf.worker.entry': 'pdfjs-dist/build/pdf.worker.js'
-    }
-}
+const webpack = require("webpack");
+const path = require("path");
 
+module.exports = {
+  entry: "./src/index.js", // Adjust the entry point according to your project
+  output: {
+    path: path.resolve(__dirname, "dist"), // Output directory
+    filename: "bundle.js", // Output filename
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply this loader to .js files
+        exclude: /node_modules/, // Exclude node_modules directory
+        use: {
+          loader: "babel-loader", // Use Babel for JavaScript files
+        },
+      },
+    ],
+  },
+  node: {
+    module: "empty", // Treat 'module' import as empty
+  },
+  plugins: [
+    // Replace original require statement for pdf.worker.js
+    new webpack.NormalModuleReplacementPlugin(
+      /worker-loader!\.\/build\/pdf\.worker\.js$/,
+      "worker-loader?esModule=false!" +
+        path.join(
+          __dirname,
+          "node_modules",
+          "pdfjs-dist",
+          "build",
+          "pdf.worker.js"
+        )
+    ),
+    // Ignore warnings about missing WASM modules
+    new webpack.IgnorePlugin({ resourceRegExp: /FetchCompileWasmPlugin$/ }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /FetchCompileAsyncWasmPlugin$/,
+    }),
+  ],
+};
